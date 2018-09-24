78507f4eb0863c657e419ac7e6655851f0691d8f
Leandro Souza <leandro.souza47@hotmail.com>
2018-09-24 20:48:57 -0300

    Inclusão da classe CSS container

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php include_once('./parts/header.php') ?>
 
-<h1>Hello World</h1>
+<?php include_once('./parts/navbar.php') ?>
+
+<div class="container">
+    <h1>Hello World</h1>
+</div>
+
+
 
 <?php include_once('./parts/footer.php') ?>
