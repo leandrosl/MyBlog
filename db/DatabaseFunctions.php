efabeb894b9fa4ce9361ceda2a9c871b8466fd42
Leandro Souza <leandro.souza47@hotmail.com>
2019-01-01 21:58:33 -0200

    Criação do método para buscar algum registro no banco pelo seu id

--- db/DatabaseFunctions.php
+++ db/DatabaseFunctions.php
@@ -7,3 +7,12 @@ function getAll($pdo, $table)
 
     return $statement->fetchAll();
 }
+
+function getById($pdo, $table, $id)
+{
+    $statement = $pdo->prepare("SELECT * FROM $table WHERE id = :id");
+    $statement->bindValue('id', $id);
+    $statement->execute();
+
+    return $statement->fetch();
+}
