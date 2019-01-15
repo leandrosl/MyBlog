b9bbf0f67c1c11dfcc50b3c297fe6db81dd5415f
Leandro Souza <leandro.souza47@hotmail.com>
2019-01-15 21:09:16 -0200

    Adicionando bootstrap ao frontend do Projeto

--- views/parts/footer.template.php
+++ views/parts/footer.template.php
@@ -1,3 +1,6 @@
+        <script src="./dist/jquery-3.0.0.slim.min.js"></script>
+        <script src="./dist/popper.min.js"></script>
+        <script src="./dist/bootstrap.min.js"></script>
         <script src="./dist/myblog.js"></script>
     </body>
 </html>
--- views/parts/header.template.php
+++ views/parts/header.template.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>MyBlog</title>
+        <link rel="stylesheet" href="./dist/bootstrap.min.css">
         <link rel="stylesheet" href="./dist/myblog.css">
     </head>
     <body>
