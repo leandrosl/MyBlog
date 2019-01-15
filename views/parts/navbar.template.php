ceb079229e70b3c7f8a35c9fb5d43384b1def32c
Leandro Souza <leandro.souza47@hotmail.com>
2019-01-15 21:20:39 -0200

    Mudança do navbar para os padrões do Bootstrap

--- views/parts/navbar.template.php
+++ views/parts/navbar.template.php
@@ -1,5 +1,20 @@
-<div class="navbar">
-    <a href="#!">Inicio</a>
-    <a href="#!">Autores</a>
-    <a href="#!">Sobre</a>
-</div>
+<nav class="navbar navbar-expand-lg">
+    <a href="#" class="navbar-brand">MyBlog</a>
+    <button class="navbar-toggler" type="button" 
+        data-toggle="collapse" data-target="#navbarSupportedContent" 
+        aria-controls="navbarSupportedContent" aria-expanded="false" 
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a href="#!" class="nav-link">Autores</a>
+            </li>
+            <li class="nav-item">
+                <a href="#!" class="nav-link">Sobre</a>
+            </li>
+        </ul>
+    </div>
+</nav>
