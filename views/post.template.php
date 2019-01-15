c50d37390d408b3f228bd545c1cc69207cf6639b
Leandro Souza <leandro.souza47@hotmail.com>
2019-01-15 21:23:36 -0200

    Mudança da página de exibição de posts com inserção de card para o conteúdo

--- views/post.template.php
+++ views/post.template.php
@@ -1,7 +1,12 @@
+<br>
 <div class="container">
     <?php if ($post): ?>
-        <h1><?= $post['title'] ?></h1>
-        <p><?= $post['post_text'] ?></p>
+        <div class="card">
+            <div class="card-body">
+                <h1><?= $post['title'] ?></h1>
+                <p><?= $post['post_text'] ?></p>
+            </div>
+        </div>
     <?php else: ?>
         <h1>Post não encontrado</h1>
     <?php endif; ?>
