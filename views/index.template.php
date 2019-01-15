ff506396f8a301f5c03c6b1cc03ec12e6abaf9d2
Leandro Souza <leandro.souza47@hotmail.com>
2019-01-15 21:27:20 -0200

    Alterações de design na página inicial

--- views/index.template.php
+++ views/index.template.php
@@ -1,10 +1,20 @@
+<br>
 <div class="container">
-    <h1>Home</h1>
-
-    <?php foreach ($posts as $post): ?>
-        <hr>
-        <h4><?= $post['title'] ?></h4>
-        <p><?= $post['post_text']?></p>
-    <?php endforeach; ?>
+    <div class="card">
+        <div class="card-body">
+        <h1>Todos os Posts</h1>
+        <br>
+        <?php foreach ($posts as $post): ?>
+            <div class="card">
+                <div class="card-body">
+                    <a href="#!">
+                        <h4><?= $post['title'] ?></h4>
+                    </a>
+                    <p><?= $post['post_text']?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
+        </div>
+    </div>
 </div>
 
