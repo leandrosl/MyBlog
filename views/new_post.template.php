473e073214be4d512ae21faa4f10614311c65cb1
Leandro Souza <leandro.souza47@hotmail.com>
2019-01-23 21:39:19 -0200

    Novo layout da página para inserção de post usando componentes do bootstrap

--- views/new_post.template.php
+++ views/new_post.template.php
@@ -1,11 +1,19 @@
-<h3>Novo Post</h3>
-
-<form method="POST">
-    <label for="title">Título</label>
-    <input type="text" id="title" name="title">
-    <br>
-    <label for="text">Texto</label>
-    <textarea name="text" id="text" cols="30" rows="10"></textarea>
-    <br>
-    <input type="submit" value="Postar">
-</form>
+<div class="container">
+<br>
+    <div class="card">
+        <div class="card-body">
+            <h3>Novo Post</h3>
+            <form action="POST">
+                <div class="form-group">
+                    <label for="title">Título</label>
+                    <input type="text" name="title" class="form-control" placeholder="Digite o título do post">
+                </div>
+                <div class="form-group">
+                    <label for="post-text">Texto</label>
+                    <textarea class="form-control" name="post-text" cols="30" rows="10"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Postar</button>
+            </form>
+        </div>
+    </div>
+</div>
