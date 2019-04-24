628a2d61485ab298133a9714e72c7633b42a786e
Leandro Souza <leandro.souza47@hotmail.com>
2019-04-23 21:50:45 -0300

    inclusão do código de criação da tabela authors no arquivo sql do projeto

--- db_scripts.sql
+++ db_scripts.sql
@@ -7,3 +7,14 @@ CREATE TABLE post (
     title VARCHAR(200),
     post_text TEXT
 );
+
+CREATE TABLE authors (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(100),
+    lastname VARCHAR(200),
+    username VARCHAR(200) UNIQUE,
+    password VARCHAR(255),
+    birth_date DATE,
+    birth_country VARCHAR(100),
+    biography VARCHAR(255)
+);
