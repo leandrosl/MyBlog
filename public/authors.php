9bbc4cde5d4176845815431b68a1c8231801c621
Leandro Souza <leandro.souza47@hotmail.com>
2019-04-23 21:42:23 -0300

    Inclusão de página e controller para exibir todos os autores cadastrados

---
+++ public/authors.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/../db/DatabaseConnection.php';
+require __DIR__ . '/../db/DatabaseFunctions.php';
+
+try {
+    $authors = getAll($pdo, 'author');
+} catch (\PDOException $e) {
+    echo "Erro ao acessar o banco de dados: " . $e->getMessage();
+}
+
+ob_start();
+
+include __DIR__ . '/../views/authors.template.php';
+
+$output = ob_get_clean();
+
+include __DIR__ . '/../views/layout.template.php';
---
+++ views/authors.template.php
@@ -0,0 +1,17 @@
+<br>
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+            <h1>Todos os autores</h1><br>
+            <?php foreach ($authors as $author): ?>
+                <div class="card">
+                    <div class="card-body">
+                        <a href="#!">
+                            <h4><?= $author['name'] .' '. $author['lastname'] ?></h4>
+                        </a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
