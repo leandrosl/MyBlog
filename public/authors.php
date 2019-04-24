b0392ff7c5535240406c512f45110695c78a0550
Leandro Souza <leandro.souza47@hotmail.com>
2019-04-23 21:50:02 -0300

    Correção do nome da tabela no método de busca de todos os autores

--- public/authors.php
+++ public/authors.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../db/DatabaseConnection.php';
 require __DIR__ . '/../db/DatabaseFunctions.php';
 
 try {
-    $authors = getAll($pdo, 'author');
+    $authors = getAll($pdo, 'authors');
 } catch (\PDOException $e) {
     echo "Erro ao acessar o banco de dados: " . $e->getMessage();
 }
