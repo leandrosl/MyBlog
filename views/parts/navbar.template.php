b0cf1b1e28ca8f4ba61466eabc939390c27d6bf4
Leandro Souza <sl.leandro09@gmail.com>
2019-09-08 21:17:52 -0300

    Inclusão dos botões para adicionar post e perfil

--- views/parts/navbar.template.php
+++ views/parts/navbar.template.php
@@ -16,5 +16,19 @@
                 <a href="#!" class="nav-link">Sobre</a>
             </li>
         </ul>
-    </div>
+        <button class="btn btn-sm btn-outline-secondary" type="button">Escreva um Post</button>
+        <div class="dropdown">
+            <button class="btn btn-success dropdown-toggle" type="button" 
+                id="dropdownMenuButton" data-toggle="dropdown" 
+                aria-haspopup="true" aria-expanded="false">
+                Nome Aqui
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Dashboard</a>
+                <a class="dropdown-item" href="#">Escreva um Post</a>
+                <a class="dropdown-item" href="#">Configurações</a>
+                <a class="dropdown-item" href="#">Logout</a>
+            </div>
+            </div>
+        </div>
 </nav>
