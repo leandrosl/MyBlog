a757a305b4b2f721f94028641c4d7935540c3399
Leandro Souza <sl.leandro09@gmail.com>
2019-10-02 22:34:52 -0300

    Mudança da importação dos arquivos Database usando require_once no post.php

--- public/post.php
+++ public/post.php
@@ -1,6 +1,6 @@
 <?php
-include_once __DIR__ . '/../db/pdoObject.php';
-include_once __DIR__ . '/../db/Database.php';
+require_once __DIR__ . '/../db/pdoObject.php';
+require_once __DIR__ . '/../db/Database.php';
 
 $database = new Database($pdo);
 
