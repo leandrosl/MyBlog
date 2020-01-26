092a8890744dc981c2ba89e9398cb2c403394d06
Leandro Souza <sl.leandro09@gmail.com>
2020-01-26 12:59:34 -0300

    Inicio da logica no arquivo login.php

--- public/login.php
+++ public/login.php
@@ -1,4 +1,26 @@
 <?php
+require_once __DIR__ . '/../db/pdoObject.php';
+require_once __DIR__ . '/../db/Database.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $database = new Database($pdo);
+
+    try {
+        $user = $database->getAllWhere('authors', [
+            'username' => $_POST["username"],
+            'password' => $_POST["password"]
+        ]);
+
+        if (is_array($user) && count($user) > 0) {
+            sleep(5);
+            header('Location: login.php');
+        } else {
+            header('Location: login.php');
+        }
+    } catch (PDOException $e) {
+        print("Erro no banco: {$e->getMessage()}");
+    }
+} else {
     ob_start();
 
     include __DIR__ . '/../views/login.template.php';
@@ -6,4 +28,5 @@
     $output = ob_get_clean();
 
     include __DIR__ . '/../views/layout.template.php';
+}
 ?>
--- views/login.template.php
+++ views/login.template.php
@@ -1,11 +1,9 @@
-
-
 <div id="login-container">
-    <form action="">
-        <label for="email">E-mail:</label>
-        <input type="email" id="email" name="email" required>
+    <form method="POST">
+        <label for="username">Usuário:</label>
+        <input type="text" id="username" name="username" required>
         <label for="password">Senha:</label>
-        <input type="password" id="password" name="password">
+        <input type="password" id="password" name="password" required>
         <div id="lower">
             <input type="checkbox"> <label for="checkbox">Continuar logado</label>
             <input type="submit" value="Entrar">
