6995acede86362c26cfacfeda08c0a39edfb13ee
Leandro Souza <sl.leandro09@gmail.com>
2020-01-26 13:10:01 -0300

    Navbar fixa e com container

--- views/parts/navbar.template.php
+++ views/parts/navbar.template.php
@@ -1,34 +1,32 @@
-<nav class="navbar navbar-expand-lg">
-    <a href="#" class="navbar-brand">MyBlog</a>
-    <button class="navbar-toggler" type="button" 
-        data-toggle="collapse" data-target="#navbarSupportedContent" 
-        aria-controls="navbarSupportedContent" aria-expanded="false" 
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar sticky-top navbar-expand-lg">
+    <div class="container">
+        <a href="#" class="navbar-brand">MyBlog</a>
+        <button class="navbar-toggler" type="button" 
+            data-toggle="collapse" data-target="#navbarSupportedContent" 
+            aria-controls="navbarSupportedContent" aria-expanded="false" 
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a href="#!" class="nav-link">Autores</a>
-            </li>
-            <li class="nav-item">
-                <a href="#!" class="nav-link">Sobre</a>
-            </li>
-        </ul>
-        <button class="btn btn-sm btn-outline-secondary" type="button">Escreva um Post</button>
-        <div class="dropdown">
-            <button class="btn btn-success dropdown-toggle" type="button" 
-                id="dropdownMenuButton" data-toggle="dropdown" 
-                aria-haspopup="true" aria-expanded="false">
-                Nome Aqui
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Dashboard</a>
-                <a class="dropdown-item" href="#">Escreva um Post</a>
-                <a class="dropdown-item" href="#">Configurações</a>
-                <a class="dropdown-item" href="#">Logout</a>
-            </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <!-- remover -->
+            </ul>
+            <button class="btn btn-sm btn-outline-secondary" type="button">Escreva um Post</button>
+            <div class="dropdown">
+                <button class="btn btn-success dropdown-toggle" type="button" 
+                    id="dropdownMenuButton" data-toggle="dropdown" 
+                    aria-haspopup="true" aria-expanded="false">
+                    Nome Aqui
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Dashboard</a>
+                    <a class="dropdown-item" href="#">Escreva um Post</a>
+                    <a class="dropdown-item" href="#">Configurações</a>
+                    <a class="dropdown-item" href="#">Logout</a>
+                </div>
+                </div>
             </div>
         </div>
+    </div>
 </nav>
